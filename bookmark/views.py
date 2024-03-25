2eeceb218071fd50e3661d2ea88234ed082ff077
jchyeon0814 <jchyeon0814@gmail.com>
2024-03-26 08:21:24 +0900

    웹사이트 리스트 기본 화면 생성

--- bookmark/views.py
+++ bookmark/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views import generic
 
-# Create your views here.
+from .models import WebSite
+
+class ListView(generic.ListView):
+    template_name = 'bookmark/list.html'
+    context_object_name = 'webSite_list'
+    
+    def get_queryset(self):
+        return WebSite.objects.order_by('id')[:6]
